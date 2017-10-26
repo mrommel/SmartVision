efc5f5b124cfd28f9cb24e6167b60f810a02e062
Michael Rommel <rommel@365farmnet.com>
2017-10-26 12:23:37 +0200

    first configuration

--- vision/models.py
+++ vision/models.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class ViewController(models.Model):
+    question_text = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
+    pub_date = models.DateTimeField('date published')
+
+
+class View(models.Model):
+    viewController = models.ForeignKey(ViewController, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
