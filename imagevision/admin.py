2dcd69e5fe29f091c2db3b5fc9737cdf47e677b6
Michael Rommel <rommel@365farmnet.com>
2018-06-15 16:04:17 +0200

    add related images

--- imagevision/admin.py
+++ imagevision/admin.py
@@ -8,7 +8,10 @@ from .models import *
 # Register your models here.
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('filename', 'title', 'folder', 'thumbnail', 'tag_list')
+	list_display = ('filename', 'title', 'folder', 'thumbnail', 'tag_list', 'detail_link',)
+	
+	fields = ('filename', 'title', 'folder', 'detailed', 'tags', 'related', 'detail_link', )
+	readonly_fields = ('detailed', 'detail_link')
 
 admin.site.register(Tag)
 admin.site.register(Image, ImageAdmin)
--- imagevision/models.py
+++ imagevision/models.py
@@ -23,6 +23,7 @@ class Image(models.Model):
 	title = models.CharField(max_length=50, blank=True, null=True)
 	folder = models.CharField(max_length=200, blank=True, null=True)
 	tags = models.ManyToManyField(Tag, blank=True, null=True)
+	related = models.ManyToManyField("self", blank=True)
 	
 	def full_path(self):
 		return "/Users/michael.rommel/365Farmnet/365Farmnet Icons/%s/%s" % (self.folder, self.filename)
@@ -30,6 +31,15 @@ class Image(models.Model):
 	def thumbnail(self):
 		return '<img border="0" alt="" src="/images/images/%s.png" height="40" />' % ((self.unique_id))
 	thumbnail.allow_tags = True
+	
+	def detailed(self):
+		return '<img border="0" alt="" src="/images/images/%s.png" height="120" />' % ((self.unique_id))
+	detailed.allow_tags = True
+	
+	def detail_link(self):
+		return u'<a href="/images/detail/%s/">%s</a>' % (self.unique_id, self)
+	detail_link.allow_tags = True
+	detail_link.short_description = "Show" 
 	
 	def tag_list(self):
 		str = ''
